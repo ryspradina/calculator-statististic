05d0f165caef275372f219ec4e6b016d8ac8d3bf
ryspradina <rias.pradinaa@gmail.com>
2022-01-24 15:53:08 +0700

    third commit

--- project_kelompok.py
+++ project_kelompok.py
@@ -3,8 +3,6 @@ import tkinter.messagebox
 import numpy as np
 import statistics
 
-# rias dan aip sayang mamah
-# aku sayang rias
 
 def rata2(angkalist) :
     return sum(angkalist) / len(angkalist)
